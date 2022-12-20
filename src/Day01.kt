f775d821d3f9082afe981850ca65fc8c25fb73a3
Emanuele Bassis <emanuele.bassis@intre.it>
2022-12-20 11:50:01 +0100

    🔥 Remove p1 function from day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,27 +3,12 @@ fun main() {
 
     val sumByGroup = input.chunkedBy { it.isBlank() }.map{ caloriesByElf -> caloriesByElf.sumOf { it.toInt() } }
 
-    fun p1(): Int {
-        var t =0;
-        var result =0;
-        input.forEach{
-            if (it.isEmpty()){
-                if (result < t)
-                    result = t
-                t = 0
-            } else
-                t += it.toInt()
-        }
-        return result
-    }
-
     fun part1(): Int = sumByGroup.max()
 
     fun part2(): Int = sumByGroup.sortedDescending()
         .take(3)
         .sum()
 
-    println(p1())
     println(part1())
     println(part2())
 }
